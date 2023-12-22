efb2b4797f1fc8ecb43706f0c5199c4704498e0f
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2023-12-22 13:09:12 +0700

    Init project

---
+++ src/main/java/SecurityWithJwt/SecurityWithJwtApplication.java
@@ -0,0 +1,13 @@
+package SecurityWithJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SercurityWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SercurityWithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SecurityWithJwt/SercurityWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package SecurityWithJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SercurityWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
